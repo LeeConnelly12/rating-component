8c5e63a30bed29fded80ec3013e3ac48ab369394
Lee Connelly <lee@leeconnelly.dev>
2022-07-17 05:59:47 +0100

    Add colors to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,17 @@ module.exports = {
       fontFamily: {
         sans: ['Overpass', ...defaultTheme.fontFamily.sans],
       },
-      colors: {},
+      colors: {
+        slate: {
+          400: 'hsl(217, 12%, 63%)',
+          500: 'hsl(216, 12%, 54%)',
+          800: 'hsl(213, 19%, 18%)',
+          900: 'hsl(216, 12%, 8%)',
+        },
+        orange: {
+          500: 'hsl(25, 97%, 53%)',
+        },
+      },
     },
   },
   plugins: [],
